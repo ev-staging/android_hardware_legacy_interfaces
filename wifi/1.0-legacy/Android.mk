LineageOS/android_hardware_interfaces

Wifi: Move LOCAL_VINTF_FRAGMENTS to executables
Current codebase has the LOCAL_VINTF_FRAGMENTS defined in the static
library definition in Android.mk. This line is skipped and does not take
effect, and hence the Wifi hal service does not start.

Moving the definition to the executable definitions, so it takes effect.

Bug: 141447327
Test: Verify that wifi starts at boot
Change-Id: I9e01483eec4017444689509ea90f761305ae471b

wifi/1.4/default/Android.mk
@@ -18,7 +18,6 @@ LOCAL_PATH := $(call my-dir)
 ###
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.wifi@1.0-service-lib
-LOCAL_VINTF_FRAGMENTS := android.hardware.wifi@1.0-service.xml
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_CPPFLAGS := -Wall -Werror -Wextra
@@ -78,6 +77,7 @@ include $(BUILD_STATIC_LIBRARY)
 ###
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.wifi@1.0-service
+LOCAL_VINTF_FRAGMENTS := android.hardware.wifi@1.0-service.xml
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_CPPFLAGS := -Wall -Werror -Wextra
@@ -107,6 +107,7 @@ include $(BUILD_EXECUTABLE)
 ###
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.wifi@1.0-service-lazy
+LOCAL_VINTF_FRAGMENTS := android.hardware.wifi@1.0-service.xml
 LOCAL_OVERRIDES_MODULES := android.hardware.wifi@1.0-service
 LOCAL_CFLAGS := -DLAZY_SERVICE
 LOCAL_MODULE_RELATIVE_PATH := hw

ev-staging/android_hardware_legacy_interfaces

wifi/1.0-legacy/Android.mk
@@ -18,7 +18,6 @@ LOCAL_PATH := $(call my-dir)
 ###
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.wifi@1.0-legacy_service-lib
-LOCAL_VINTF_FRAGMENTS := android.hardware.wifi@1.0-legacy_service.xml
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_CPPFLAGS := -Wall -Werror -Wextra
@@ -78,6 +77,7 @@ include $(BUILD_STATIC_LIBRARY)
 ###
 include $(CLEAR_VARS)
 LOCAL_MODULE := android.hardware.wifi@1.0-service.legacy
+LOCAL_VINTF_FRAGMENTS := android.hardware.wifi@1.0-legacy_service.xml
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_CPPFLAGS := -Wall -Werror -Wextra